60bcec1ff756c0147cb75460640f19c66f86ccce
Gulfiya Isyangulova <isyangulova2013@gmail.com>
2019-07-31 21:56:54 -0400

    React Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route, Link, Switch} from 'react-router-dom';
+import Signup from './components/user/Signup.js';
+import Login from './components/user/Login.js';
+import Navbar from './components/Navbar.js'
+import GoogleMap from './components/GoogleMap.js';
 
-function App() {
+import AuthService from './services/AuthService.js';
+
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = { loggedInUser: null };
+  }
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+
+<GoogleMap />
+<Navbar 
+theUser = {this.state.currentlyLoggedIn} 
+pleaseLogOut = {()=> this.service.logout()}
+toggleForm = {this.toggleForm}
+getUser = {this.getCurrentlyLoggedInUser}
+
+/>
+
+{this.state.signupShowing && 
+  <Signup getUser = {this.getCurrentlyLoggedInUser}
+  toggleForm = {this.toggleForm}
+   />
+}
+
+{this.state.loginShowing && 
+  <Login getUser = {this.getCurrentlyLoggedInUser}
+  toggleForm = {this.toggleForm}
+  />
+}
+      Jiu-Jitsu app
     </div>
   );
 }
-
+}
 export default App;
